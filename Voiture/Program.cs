c92357686933799ae668f26bbc12c0773bbc637f
Yani Meziani <yani.meziani@protonmail.com>
2023-06-06 09:05:23 -0400

    Création de la classe voiture terminée. Début de la création du Main.

---
+++ Voiture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Voiture
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
---
+++ Voiture/Voiture.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace Voiture
+{
+
+    public class Voiture
+    {
+        private string marque;
+        private string modele;
+        private double vitesse;
+        private bool estDemarre;
+
+        public string Marque
+        {
+            get { return marque; }
+            set { marque = value; }
+        }
+        public string Modele
+        {
+            get { return modele; }
+            set { modele = value; }
+        }
+        public double Vitesse
+        {
+            get { return vitesse; }
+            set { vitesse = value; }
+        }
+        public bool EstDemarre
+        {
+            get { return estDemarre; }
+            set { estDemarre = value; }
+        }
+
+        public Voiture(string marque, string modele)
+        {
+            this.Marque = marque;
+            this.Modele = modele;
+        }
+
+        public string ObtenirIdentifiant()
+        {
+            return $"[ {this.Marque} | {this.Modele} ]";
+        }
+
+        public void Demarrer()
+        {
+            if (!this.EstDemarre)
+            {
+                this.EstDemarre = true;
+            }
+            else
+            {
+                Console.WriteLine($"{ObtenirIdentifiant()} Véhicule déja en marche.");
+            }
+
+        }
+
+        public void Arreter()
+        {
+            if (this.EstDemarre)
+            {
+                this.EstDemarre = false;
+            }
+            else
+            {
+                Console.WriteLine($"{ObtenirIdentifiant()} Véhicule déja à l'arrêt.");
+            }
+        }
+
+        public void Accelerer(double vitesseGagnee)
+        {
+            this.Vitesse += vitesseGagnee;
+            Console.WriteLine($"{ObtenirIdentifiant()} Véhicule maintenant à la vitesse : {this.Vitesse} km/h.");
+        }
+
+        public void Freiner(double vitessePerdue)
+        {
+            this.Vitesse -= vitessePerdue;
+            Console.WriteLine($"{ObtenirIdentifiant()} Véhicule maintenant à la vitesse : {this.Vitesse} km/h.");
+        }
+
+        public override string ToString()
+        {
+            return $"Marque : {this.Marque} \nModèle : {this.Modele} \nVitesse : {this.Vitesse} \nEst démarrée : {this.EstDemarre}";
+        }
+    }
+}
+
